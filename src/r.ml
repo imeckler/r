7d5c5d5cb2505fb7976157ecff7ef383d69bd795
Izaak Meckler <ihmeckler@gmail.com>
2019-06-04 21:50:01 -0700

    remove comment

--- src/r.ml
+++ src/r.ml
@@ -1,12 +1,1 @@
-(* Maybe you give a quantity like
-
-   r 1 'test'
-
-   gives one bit, one alpha, one number, an ordering on 1 object, etc.
-
-   r perm these are the words
-
-   permute them randomly?
-   *)
-
 let () = Core.Command.run Cmd.command
